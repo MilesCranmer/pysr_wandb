233bf32a2713bbe923982417dc28aac167888aa1
MilesCranmer <miles.cranmer@gmail.com>
2022-11-07 03:11:51 -0500

    Create runner file

--- pysr_wandb/pysr_wandb.py
+++ pysr_wandb/pysr_wandb.py
@@ -1,1 +1,123 @@
 """Main module."""
+import wandb
+from pysr import PySRRegressor
+from .problems import *
+
+UNCHANGED_PARAMS = dict(
+    niterations=10000000,
+    model_selection="best",
+    binary_operators=["+", "-", "*", "/"],
+    unary_operators=["sin", "cos", "exp", "square", "cube"],
+    max_evals=None,
+    maxdepth=None,
+    maxsize=30,
+    constraints=None,
+    nested_constraints=None,
+    complexity_of_operators=None,
+    complexity_of_constants=1,
+    complexity_of_variables=1,
+    loss="L2DistLoss()",
+    use_frequency=True,
+    use_frequency_in_tournament=True,
+    early_stop_condition="early_stop(loss, complexity) = loss < 1e-15 && complexity < 20",
+    skip_mutation_failures=True,
+    migration=True,
+    hof_migration=True,
+    optimizer_algorithm="BFGS",
+    should_optimize_constants=True,
+    multithreading=True,
+    update=False,
+    procs="REQUIRED",  # User needs to override this.
+    progress=False,
+    verbosity=0,
+    warm_start=False,
+    turbo=True,
+    precision=32,
+    fast_cycle=False,
+    random_state=None,
+    deterministic=False,
+)
+
+timeout_minutes = 3
+
+DEFAULT_CONFIG = dict(
+    timeout_in_seconds=timeout_minutes * 60,
+    populations=15,
+    population_size=33,
+    warmup_maxsize_by=0.0,
+    parsimony=0.0032,
+    adaptive_parsimony_scaling=20.0,
+    alpha=0.1,
+    annealing=False,
+    ncyclesperiteration=550,
+    fraction_replaced=0.000364,
+    fraction_replaced_hof=0.035,
+    weight_add_node=0.79,
+    weight_insert_node=5.1,
+    weight_delete_node=1.7,
+    weight_do_nothing=0.21,
+    weight_mutate_constant=0.048,
+    weight_mutate_operator=0.47,
+    weight_randomize=0.00023,
+    weight_simplify=0.0020,
+    weight_optimize=0.0,
+    crossover_probability=0.066,
+    topn=12,
+    optimizer_nrestarts=2,
+    optimize_probability=0.14,
+    optimizer_iterations=8,
+    perturbation_factor=0.076,
+    tournament_selection_n=10,
+    tournament_selection_p=0.86,
+    batching=False,
+    batch_size=50,
+)
+
+
+def init_wandb():
+    wandb.init(
+        project="pysr_wandb",
+        config=DEFAULT_CONFIG,
+    )
+    return wandb
+
+
+def run(wandb, problem, seed):
+    X, y = problem(seed)
+    model = PySRRegressor(**UNCHANGED_PARAMS)
+    model.set_params(**wandb.config)
+    model.set_params(seed=seed)
+    model.fit(X, y)
+    best_loss = model.best().iloc[-1].loss
+    best_complexity = model.best().iloc[-1].complexity
+    combined = best_loss**0.2 + best_complexity * 0.01
+
+    problem_name = problem.__name__
+    wandb.log(
+        {
+            f"loss_{problem_name}_{seed}": best_loss,
+            f"complexity_{problem_name}_{seed}": best_complexity,
+            f"combined_{problem_name}_{seed}": combined,
+        }
+    )
+    return combined
+
+
+def runall(wandb):
+    all_results = []
+    for problem in PROBLEMS:
+        combined = []
+        for seed in range(5):
+            combined.append(run(wandb, problem, seed))
+        med = np.median(combined)
+        wandb.log(
+            {
+                f"combined_{problem.__name__}": med,
+            }
+        )
+        all_results.append(med)
+    wandb.log(
+        {
+            "overall": np.median(all_results),
+        }
+    )
